f8bd271b2ad2e698a484367fce0152d428ab1309
camshaft <bytheway.cameron@gmail.com>
2014-07-05 01:34:56 -0600

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ fproxy-server
 =============
 
 A simple forward proxy run on heroku
+
+Installation
+------------
+
+```sh
+$ npm install --save camshaft/fproxy-server
+```
+
+Usage
+-----
+
+```js
+var proxy = require('fproxy-server');
+
+var app = module.exports = proxy('my-username', 'my-password');
+```
+
+This is a plain HTTP/HTTPS forward server with the exception of the `host` header. Heroku uses this header to route to the different applications so it cannot be used. The `x-host` header is used instead. If a client does not support the `x-host` header try pairing the setup with [fproxy-client](https://github.com/camshaft/fproxy-client).
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var proxy = require('simple-http-proxy');
+
+module.exports = function(user, pass) {
+  var app = express();
+
+  user = user || process.env.PROXY_USERNAME;
+  pass = pass || process.env.PROXY_PASSWORD;
+
+  var auth = express.basicAuth(user, pass);
+  app.use(function(req, res, next) {
+    var authHeader = req.headers.authorization;
+    req.headers.authorization = req.headers['proxy-authorization'] || authHeader;
+    delete req.headers['proxy-authorization'];
+    auth(req, res, function() {
+      req.headers.authorization = authHeader;
+      next.apply(null, arguments);
+    });
+  });
+
+  app.use(function(req, res, next) {
+    var host = req.headers['x-host'];
+    var proto = req.headers['x-forwarded-proto'] || 'http';
+    delete req.headers.host;
+    delete req.headers['x-forwarded-proto'];
+    proxy(proto + '://' + host, {timeout: false})(req, res, next);
+  });
+
+  return app;
+};
